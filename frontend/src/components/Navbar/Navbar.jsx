9b8e8b3828c001de5d9c04eaea393543edd7d8b1
FREDERIC TICON <fred.ticon@gmail.com>
2024-01-31 12:29:15 +0100

    Create Navbar.jsx

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import "./Navbar.scss";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul className="navbar__ul">
+        <li className="navbar__li">
+          <a className="navbar__a" href="#">
+            Acceuil
+          </a>
+        </li>
+        <li className="navbar__li">
+          <a className="navbar__a" href="#">
+            Ateliers
+          </a>
+        </li>
+        <li className="navbar__li">
+          <a className="navbar__a" href="#">
+            Catalogue
+          </a>
+        </li>
+        <li className="navbar__li">
+          <a className="navbar__a" href="#">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
