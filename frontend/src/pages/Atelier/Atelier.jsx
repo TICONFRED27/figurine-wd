a549bb2486b94841f6cd22cdd398fb1a32bbe312
FREDERIC TICON <fred.ticon@gmail.com>
2024-01-31 15:02:53 +0100

    Create Atelier.jsx

---
+++ frontend/src/pages/Atelier/Atelier.jsx
@@ -0,0 +1,14 @@
+import "./Atelier.scss";
+import Navbar from "../../components/Navbar/Navbar";
+
+function Atelier() {
+  return (
+    <div className="contenu">
+      <h1 className="title">Fallen Figurines game</h1>
+      <Navbar />
+      <h2 className="title">Ateliers</h2>
+    </div>
+  );
+}
+
+export default Atelier;
