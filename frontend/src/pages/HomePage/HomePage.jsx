cd9d015ad7a54cfccc5beeba28438086235ca9c2
FREDERIC TICON <fred.ticon@gmail.com>
2024-02-01 14:27:37 +0100

    Update HomePage.jsx

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -20,22 +20,31 @@ function HomePage() {
           />
         </div>
       </div>
-
-      <p className="descriptionJeux">
-        "The Walking Dead: All Out War" est un jeu de plateau passionnant basé
-        sur la célèbre série de bandes dessinées "The Walking Dead" de Robert
-        Kirkman. Ce jeu plonge les joueurs dans un univers post-apocalyptique où
-        les morts-vivants errent dans un monde ravagé. Concept du Jeu Dans "The
-        Walking Dead: All Out War", les joueurs prennent le rôle de survivants
-        luttant contre les hordes de zombies, mais aussi contre d'autres groupes
-        de survivants. Avec un système de jeu basé sur des dés et des cartes,
-        les joueurs doivent gérer leurs ressources, planifier leurs mouvements
-        et combattre pour leur survie. Conclusion Pour les amateurs de la série
-        et les passionnés de jeux de stratégie, "The Walking Dead: All Out War"
-        est un incontournable. Il offre une expérience de jeu captivante et une
-        rejouabilité élevée grâce à ses nombreux scénarios et extensions
-        disponibles.
-      </p>
+      <div className="container1">
+        <p className="descriptionJeux">
+          "The Walking Dead: All Out War" est un jeu de plateau passionnant basé
+          sur la célèbre série de bandes dessinées "The Walking Dead" de Robert
+          Kirkman. Ce jeu plonge les joueurs dans un univers post-apocalyptique
+          où les morts-vivants errent dans un monde ravagé. Concept du Jeu Dans
+          "The Walking Dead: All Out War", les joueurs prennent le rôle de
+          survivants luttant contre les hordes de zombies, mais aussi contre
+          d'autres groupes de survivants. Avec un système de jeu basé sur des
+          dés et des cartes, les joueurs doivent gérer leurs ressources,
+          planifier leurs mouvements et combattre pour leur survie. Conclusion
+          Pour les amateurs de la série et les passionnés de jeux de stratégie,
+          "The Walking Dead: All Out War" est un incontournable. Il offre une
+          expérience de jeu captivante et une rejouabilité élevée grâce à ses
+          nombreux scénarios et extensions disponibles.
+        </p>
+        <div className="boite1">
+          <h2 className="titreBoite1">Zombicide nouvelle sortie</h2>
+          <img
+            src="src/assets/boitezombicide.webp"
+            alt=""
+            className="boiteJeuxzomb"
+          />
+        </div>
+      </div>
     </div>
   );
 }
