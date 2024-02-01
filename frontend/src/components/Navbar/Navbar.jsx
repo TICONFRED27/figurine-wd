61b1fec4f18aba3339bcb87a83ac47ca378d9baf
FREDERIC TICON <fred.ticon@gmail.com>
2024-02-01 17:06:15 +0100

    Update Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,25 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { Link } from "react-router-dom";
 import "./Navbar.scss";
 
 function Navbar() {
   return (
     <nav className="navbar">
       <ul className="navbar__ul">
-        <li className="navbar__li">
-          <a className="navbar__a" href="#Acceuil">
-            Acceuil
-          </a>
-        </li>
-        <li className="navbar__li">
-          <a className="navbar__a" href="#">
-            Atelier
-          </a>
-        </li>
+        <Link to="/">
+          <li className="navbar__li">
+            <a className="navbar__a" href="">
+              Accueil
+            </a>
+          </li>
+        </Link>
+        <Link to="/atelier">
+          <li className="navbar__li">
+            <a className="navbar__a" href="">
+              Atelier
+            </a>
+          </li>
+        </Link>
         <li className="navbar__li">
           <a className="navbar__a" href="#Catalogue">
             Catalogue
