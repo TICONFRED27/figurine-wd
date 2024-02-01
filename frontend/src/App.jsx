4c8046d421e8e564e80ccf514ce897c4af24e10f
FREDERIC TICON <fred.ticon@gmail.com>
2024-02-01 17:19:44 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import "./App.css";
 import React from "react";
 import HomePage from "./pages/HomePage/HomePage";
-import Atelier from "./pages/Atelier/Atelier";
 
 function App() {
   return (
     <>
       <HomePage />;
-      <Atelier />;
     </>
   );
 }
