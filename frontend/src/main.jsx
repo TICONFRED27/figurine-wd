d5f992852ad1259c6bbc43e3f7e94b7542a5ad9f
FREDERIC TICON <fred.ticon@gmail.com>
2024-02-01 17:06:11 +0100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 import App from "./App";
+import Atelier from "./pages/Atelier/Atelier";
+import HomePage from "./pages/HomePage/HomePage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/homepage",
+    element: <HomePage />,
+  },
+  {
+    path: "/atelier",
+    element: <Atelier />,
   },
 ]);
 
