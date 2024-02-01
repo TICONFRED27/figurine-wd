5c044550f5f04c3ba7ba1565336bc3edc1081ad4
FREDERIC TICON <fred.ticon@gmail.com>
2024-02-01 15:30:18 +0100

    Update Atelier.jsx

--- frontend/src/pages/Atelier/Atelier.jsx
+++ frontend/src/pages/Atelier/Atelier.jsx
@@ -39,14 +39,80 @@ function Atelier() {
         voir parmi nous et de découvrir ensemble l'art extraordinaire de la
         peinture de figurines et de la création de décors en maquette!
       </p>
-      <img
-        src="src/assets/personnequi peint.webp"
-        alt=""
-        className="perspeindre"
-      />
-      <p>rgojtorbgij</p>
-      <img src="src/assets/personnedecord.webp" alt="" className="decors" />
-      <p>rgojtorbgij</p>
+      <div className="containerPeindre">
+        <img
+          src="src/assets/personnequi peint.webp"
+          alt=""
+          className="perspeindre"
+        />
+        <div className="lesAteliers">
+          <h2 className="titreAteliers2">
+            Découvrez l'Art de la Peinture sur Figurine
+          </h2>
+          <p className="textExp">
+            Ateliers Créatifs pour Tous Bienvenue dans l'univers passionnant de
+            la peinture sur figurine ! Nous sommes ravis de vous proposer une
+            série d'ateliers destinés à tous les amateurs d'art, de modélisme et
+            de jeux de plateau. Que vous soyez débutant ou peintre expérimenté,
+            nos ateliers offrent une occasion unique de développer vos
+            compétences en peinture miniature. Pourquoi Participer ?
+            Développement de la créativité : La peinture sur figurine est une
+            forme d'art qui stimule l'imagination. Chaque pièce devient une
+            toile où s'exprime votre créativité. Apprentissage technique : De la
+            préparation des figurines à la maîtrise des différentes techniques
+            de peinture, vous apprendrez tout ce qu'il faut savoir pour réaliser
+            des œuvres miniatures étonnantes. Ambiance conviviale : Rejoignez
+            une communauté de passionnés. Nos ateliers sont des moments de
+            partage et d'échange autour d'une passion commune. Détente et
+            bien-être : La peinture sur figurine est une activité relaxante.
+            Elle permet de se concentrer sur le moment présent et d'oublier les
+            tracas du quotidien. Nos Ateliers : Initiation à la Peinture sur
+            Figurine : Découvrez les bases de la peinture sur miniature. Idéal
+            pour les débutants. Techniques Avancées : Pour ceux qui souhaitent
+            perfectionner leurs compétences, cet atelier se concentre sur des
+            techniques plus complexes comme le lavis, le brossage à sec, et plus
+            encore. Atelier Thématique : Plongez dans des univers variés
+            (fantastique, historique, science-fiction) et apprenez à peindre des
+            figurines spécifiques à chaque thème. Atelier Libre : Un espace pour
+            travailler sur vos projets personnels avec des conseils
+            professionnels à disposition.
+          </p>
+        </div>
+      </div>
+      <div className="containerDecors">
+        <img src="src/assets/personnedecord.webp" alt="" className="decors" />
+        <div className="lesAteliers">
+          <h2 className="titreAteliers2">
+            Ateliers de Création de Décors pour Jeux de Plateaux
+          </h2>
+          <p className="textExp">
+            Titre : Amoureux des jeux de plateaux et passionnés de modélisme,
+            nous avons le plaisir de vous annoncer le lancement de nos ateliers
+            dédiés à la création de décors uniques et personnalisés. Ces
+            sessions sont conçues pour enrichir votre expérience ludique et
+            donner vie à vos univers de jeux. Pourquoi Rejoindre nos Ateliers ?
+            Créativité et Personnalisation : Apprenez à créer des décors qui
+            reflètent votre style et votre vision. Chaque création est unique,
+            rendant vos parties de jeux encore plus immersives. Techniques de
+            Modélisme : Que vous soyez débutant ou expérimenté, nos ateliers
+            couvrent une gamme de techniques, depuis la conception basique
+            jusqu’aux finitions complexes. Ambiance Collaborative : Partagez
+            votre passion avec une communauté de joueurs et de créateurs. Nos
+            ateliers sont des espaces d’échange et d’inspiration. Amélioration
+            de l'Expérience de Jeu : Transformez vos jeux de plateaux avec des
+            décors qui ajoutent une dimension visuelle et tactile à vos parties.
+            Nos Ateliers Proposent : Atelier de Base : Apprenez les fondamentaux
+            de la création de décors – choix des matériaux, construction de
+            base, peinture et finitions. Atelier Thématique : Créez des décors
+            spécifiques à différents types de jeux – fantasy, historique,
+            futuriste, et plus encore. Atelier Avancé : Pour ceux qui souhaitent
+            pousser leur art plus loin, cet atelier se concentre sur des
+            techniques complexes et des effets spéciaux. Sessions de Travail
+            Libre : Un espace pour développer vos projets personnels avec des
+            conseils d'experts à votre disposition.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
